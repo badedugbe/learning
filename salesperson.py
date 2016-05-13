b069959d350db672db46a5b449b8b8aa5e0f1913
Jerry Adedugbe <badedugbe@gmail.com>
2016-05-13 18:33:05 +0100

    i added the if statement here and it made it very crappy

---
+++ salesperson.py
@@ -0,0 +1,68 @@
+"""
+salesperson assignment
+"""
+
+#salesperson's ID
+
+salesperson = {'a':1, 'b':2, 'c':3, 'd':4}
+
+# product number
+
+productid = {'pa':1, 'pb':2, 'pc':3, 'pd':4, 'pe':5}
+
+# product's amount
+
+products = {
+	'1': 100, '2':200, '3':50, '4':129, '5':100
+}
+
+# number of product sold
+
+numprodsold = ''
+
+sa = raw_input('input salesperson "a" number, productid and numprodsold at once. e.g 1 3 100\n')
+ssa = sa.split()
+sar = int(ssa[2]) * products[ssa[1]]
+sb = raw_input('input salesperson "b" number, productid and numprodsold at once. e.g 1 3 100\n')
+ssb = sb.split()
+if ssb[1] == ssa[1]:
+    print 'product has been taken'
+sb = raw_input('input salesperson "b" number, productid and numprodsold again. e.g 1 3 100\n')
+ssb = sb.split()
+sbr = int(ssb[2]) * products[ssb[1]]
+sc = raw_input('input salesperson "c" number, productid and numprodsold at once. e.g 1 3 100\n')
+ssc = sc.split()
+if ssc[1] == ssa[1]:
+    print 'product has been taken'
+sc = raw_input('input salesperson "c" number, productid and numprodsold again. e.g 1 3 100\n')
+ssc = sc.split()
+if ssc[1] == ssb[1]:
+    print 'product has been taken'
+sc = raw_input('input salesperson "c" number, productid and numprodsold again. e.g 1 3 100\n')
+ssc = sc.split()
+scr = int(ssc[2]) * products[ssc[1]]
+sd = raw_input('input salesperson "d" number, productid and numprodsold at once. e.g 1 3 100\n')
+ssd = sd.split()
+if ssd[1] == ssa[1]:
+    print 'product has been taken'
+sd = raw_input('input salesperson "d" number, productid and numprodsold again. e.g 1 3 100\n')
+ssd = sd.split()
+if ssd[1] == ssb[1]:
+    print 'product has been taken'
+sd = raw_input('input salesperson "d" number, productid and numprodsold again. e.g 1 3 100\n')
+ssd = sd.split()
+if ssd[1] == ssc[1]:
+    print 'product has been taken'
+sd = raw_input('input salesperson "d" number, productid and numprodsold again. e.g 1 3 100\n')
+ssd = sd.split()
+sdr = int(ssd[2]) * products[ssd[1]]
+
+print "salesperson %d sold %d copies of item %s which equals %d" % (int(ssa[0]), int(ssa[2]), int(ssa[1]), sar)
+print "salesperson %d sold %d copies of item %s which equals %d" % (int(ssb[0]), int(ssb[2]), int(ssb[1]), sbr)
+print "salesperson %d sold %d copies of item %s which equals %d" % (int(ssc[0]), int(ssc[2]), int(ssc[1]), scr)
+print "salesperson %d sold %d copies of item %s which equals %d" % (int(ssd[0]), int(ssd[2]), int(ssd[1]), sdr)
+
+result = sar + sbr + scr + sdr
+
+
+print "Total sales for the day is", result
